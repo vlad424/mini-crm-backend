9ae49e8d038c9508b836c2e74c2d819305a0cda2
vlad424 <vlad6754687@gmail.com>
2024-06-29 15:08:25 +0700

    not work cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({credentials: true, origin: "http://95.163.223.141:3000/"});
+  app.enableCors();
 
   await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
 }
