0005edb452d39e7cc5fed1f2e10bdca84575a785
vlad424 <vlad6754687@gmail.com>
2024-06-29 18:15:29 +0700

    added http status

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,31 +1,38 @@
-import { BadGatewayException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadGatewayException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotAcceptableException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CredentialsDto } from './auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma : PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   async login(credentials: CredentialsDto) {
-    if(credentials.isCa === true) {
+    if (credentials.isCa === true) {
       const counterAgent = await this.prisma.counterAgent.findFirst({
-        where: {INN: credentials.login},
+        where: { INN: credentials.login },
         include: {
-          role: true
-        }
-      })
-      if(!counterAgent) throw new BadGatewayException()
+          role: true,
+        },
+      });
+      if (!counterAgent) throw new BadGatewayException();
 
-      return {
-        ...counterAgent,
-        role: counterAgent.role.name_role
-        
-      }
-    }
+      if (counterAgent.password === credentials.password) {
+        return {
+          ...counterAgent,
+          role: counterAgent.role.name_role,
+        };
+      } else throw new HttpException('NON AUTH', HttpStatus.NON_AUTHORITATIVE_INFORMATION);
+    } 
     else {
       const admin = await this.prisma.admin.findUnique({
         where: {
-          login: credentials.login
+          login: credentials.login,
         },
         select: {
           lastName: true,
@@ -33,20 +40,24 @@ export class AuthService {
           login: true,
           role: true,
           id: true,
-          uids: true
-        }
-      })
-  
-      if(!admin) throw new BadGatewayException()
-  
-      return {
-        lastName: admin.lastName,
-        firstName: admin.firstName,
-        login: admin.login,
-        role: admin.role.name_role,
-        id: admin.id,
-        uids: admin.uids
+          uids: true,
+          password: true
+        },
+      });
+
+      if (!admin) throw new BadGatewayException();
+
+      if (admin.password === credentials.password) {
+        return {
+          lastName: admin.lastName,
+          firstName: admin.firstName,
+          login: admin.login,
+          role: admin.role.name_role,
+          id: admin.id,
+          uids: admin.uids,
+        };
       }
+      else throw new HttpException('NON AUTH', HttpStatus.NON_AUTHORITATIVE_INFORMATION);
     }
   }
 }
--- src/counteragent/counteragent.controller.ts
+++ src/counteragent/counteragent.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { CounteragentService } from './counteragent.service';
 import { AddCounterAgentDto } from './couteragent.dto';
 
@@ -6,14 +6,17 @@ import { AddCounterAgentDto } from './couteragent.dto';
 export class CounteragentController {
   constructor(private readonly counteragentService: CounteragentService) {}
 
+  @HttpCode(200)
   @Post(':id')
   async addCounteragent(@Body() counterAgentData : AddCounterAgentDto) {
     return this.counteragentService.addCounteragent(counterAgentData)
   }
+  @HttpCode(200)
   @Get(':id')
   async getCounterAgents(@Param() adminId: {id: number}) {
     return this.counteragentService.getCounterAgents(adminId.id)
   }
+  @HttpCode(200)
   @Patch(':id')
   async acceptOrder(@Body() data: {orderId: number, adminId: number}) {
     return this.counteragentService.acceptOrder(data)
