09601a32a8124cea3ba93621c0a691a6b8ad4060
RAMP <54576715+vlad424@users.noreply.github.com>
2024-06-29 10:11:13 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders:"*",
-    origin: "*"
-  });
+  app.enableCors({credentials: true, origin: "http://95.163.223.141:3000/"});
 
   await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
 }
