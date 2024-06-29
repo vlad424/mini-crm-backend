be818360e9305762a118a7aad4ed35cb4fa2004a
RAMP <54576715+vlad424@users.noreply.github.com>
2024-06-29 09:57:13 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,17 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
 
-    var whitelist = ['http://95.163.223.141:3000', 'http://95.163.223.141:3000'];
-app.enableCors({
-origin: function (origin, callback) {
-  if (whitelist.indexOf(origin) !== -1) {
-    console.log("allowed cors for:", origin)
-    callback(null, true)
-  } else {
-    console.log("blocked cors for:", origin)
-    callback(new Error('Not allowed by CORS'))
-  }
-},
-allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-credentials: true,
-});
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+  });
 
   await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
 }
