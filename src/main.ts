b17ef828884aaf7bf2b756f61c14a5de5b15ec5b
vlad424 <vlad6754687@gmail.com>
2024-07-01 17:53:09 +0700

    try find error

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000
-  const app = await NestFactory.create(AppModule, {
-    cors: true
-  });
+  const app = await NestFactory.create(AppModule);
 
-  await app.listen(PORT, () => console.log(`server start on ${PORT} port`));
+  app.enableCors()
+
+  await app.listen(5000, () => console.log(`server start on ${5000} port`));
 }
 bootstrap();
