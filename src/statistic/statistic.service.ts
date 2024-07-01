13c0477f6b7c4fdecbdb79c830a6b066e8654225
vlad424 <vlad6754687@gmail.com>
2024-07-01 21:24:43 +0700

    commit

--- src/statistic/statistic.service.ts
+++ src/statistic/statistic.service.ts
@@ -6,7 +6,7 @@ export class StatisticService {
   constructor(private prsima : PrismaService) {}
 
   async getStatistic(adminId : number) {
-    const all_orders = await this.prsima.statistic.findMany({
+    let all_orders : any = await this.prsima.statistic.findMany({
       where: {
         adminId: +adminId
       },
@@ -15,10 +15,23 @@ export class StatisticService {
         expenses: true,
         incomes: true,
         products: true,
-        counterAgentId: true
+        counterAgentId: true,
       },
     })
 
-    return all_orders 
+    for(let i = 0; i < all_orders.length; i++) {
+      const user_name = await this.prsima.counterAgent.findUnique({
+        where: {
+          id: +all_orders[i].counterAgentId
+        },
+        select: {
+          name: true
+        }
+      })
+
+      all_orders[i].counterAgentId = user_name.name
+    }
+
+    return all_orders
   }
 }
